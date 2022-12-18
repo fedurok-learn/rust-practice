323031e8783c259e7f0a76b14c23b19249d8856b
Mykola Fedurko <kolausf@gmail.com>
2022-12-18 18:54:39 +0200

    Provided Option-based solution.

--- list/src/main.rs
+++ list/src/main.rs
@@ -1,96 +1,55 @@
 use std::fmt;
+use std::mem;
 
-#[derive(Clone)]
-enum Address {
-  None,
-  Some(Box<Node>),
+type Address = Option<Box<Node>>;
+
+struct List {
+  head: Address,
 }
 
-impl Address {
-  pub fn val_addr(val: i32, addr: Address) -> Address {
-    Self::Some(Box::new(Node { val: val, next: addr }))
-  } 
-
-  pub fn val(val: i32) -> Address {
-    Self::val_addr(val, Self::None)
-  }
-
-  pub fn empty() -> Address { Self::None }
-}
-
-#[derive(Clone)]
 struct Node {
   val: i32,
   next: Address
 }
 
-impl Node {
+impl List {
+  pub fn new() -> Self {
+    Self { head: None }
+  } 
+  
   pub fn push(&mut self, val: i32) {
-    match self.next {
-      Address::None => self.next = Address::val(val),
-      Address::Some(ref mut next) => next.push(val),
-    }
+    self.head = Some(Box::new(Node { 
+      val: val, 
+      next: self.head.take() 
+    }));
   }
 
-  pub fn contains(&self, val: i32) -> bool {
-    if val == self.val { 
-      true 
-    } else {
-      match self.next {
-        Address::None => false,
-        Address::Some(ref next) => next.contains(val),
-      }
-    }
-  }
-
-  pub fn remove(&mut self, val: i32) -> bool {
-    match self.next {
-      Address::None => false,
-      Address::Some(ref mut next) => {
-        if val == next.val {
-          self.next = next.next.clone();
-          true
-        } else {
-          next.remove(val);   
-          false
-        }
-      }
-    }
+  pub fn pop(&mut self) -> Option<i32> {
+    self.head.take().map(|nd| {
+      self.head = nd.next;
+      nd.val
+    })
   }
 
   pub fn print(&self) {
-    self._print();
-    println!();
-  }
-
-  fn _print(&self) {
-    print!("{} ", self.val);
-    match self.next {
-      Address::None => return,
-      Address::Some(ref next) => next._print(),
+    let mut cur_link = &self.head;
+    while let Some(ref nd) = cur_link {
+      print!("{} ", nd.val);
+      cur_link = &nd.next;
     }
-  }
-}
-
-impl fmt::Display for Node {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "(todo Node print)")
+    println!();
   }
 }
 
 fn main() {
-  let mut root = Node { val: 1, next: Address::None }; 
-  root.push(2);
-  root.push(3);
-  root.push(4);
-  root.push(5);
-  root.print();
-  root.remove(4);
-  root.print();
-
-  for el in 0..=5 {
-    println!("el: {}, contains: {}", el, root.contains(el));
-  }
-
-  println!("{root}");
+  let mut list = List::new();
+  list.push(2);
+  list.push(3);
+  list.push(4);
+  list.push(5);
+  list.print();
+  list.pop();
+  list.print();
+  list.pop();
+  list.print();
 }
